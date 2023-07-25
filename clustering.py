6c0f2f16ebded9b1ad6537419d2eb58d761f1463
Eddie Han <eddieseunghunhan@gmail.com>
2023-07-25 20:02:55 +0900

    major revision WIP
    because versioning is hard - currently removed all other unnecessary files and focusing on getting clustering/mlp code to work

---
+++ clustering.py
@@ -0,0 +1,149 @@
+import argparse
+import os
+import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+
+def read_data(input_folder):
+    data_dict = {}
+    for key in os.listdir(input_folder):
+        path = os.path.join(input_folder, key)
+        data = []
+        for file in os.listdir(path):
+            with open(os.path.join(path, file), "r") as f:
+                try:
+                    data.append(json.load(f))
+                except json.JSONDecodeError:
+                    print(f"Error decoding JSON in file: {file}")
+        data_dict[key] = data
+    return data_dict
+
+
+def preprocess_data(df):
+    num_features = len(df.columns) - 1
+    column_names = [f"feature{i+1}" for i in range(num_features)] + ["key"]
+    df.columns = column_names
+
+    scaler = StandardScaler()
+    df.iloc[:, :-1] = scaler.fit_transform(df.iloc[:, :-1])
+
+    return df
+
+
+def plot_2d_pca(finalDf, targets, colors, folder, basename):
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel('Principal Component 1', fontsize=15)
+    ax.set_ylabel('Principal Component 2', fontsize=15)
+    ax.set_title('2 component PCA', fontsize=20)
+
+    for target, color in zip(targets, colors):
+        indicesToKeep = finalDf['key'] == target
+        ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1'],
+                   finalDf.loc[indicesToKeep, 'principal component 2'],
+                   color=color,
+                   s=50)
+    ax.legend(targets)
+    ax.grid()
+
+    os.makedirs(folder, exist_ok=True)
+
+    counter = 1
+    filename = f'{basename}.png'
+    while os.path.exists(os.path.join(folder, filename)):
+        counter += 1
+        filename = f'{basename}_{counter}.png'
+
+    fig.savefig(os.path.join(folder, filename))
+
+    pca_output = finalDf[['principal component 1', 'principal component 2']].values
+    np.savetxt(os.path.join(folder, f'{basename}_pca_output.csv'), pca_output, delimiter=',')
+
+    explained_variance_ratios = pca.explained_variance_ratio_
+    np.savetxt(os.path.join(folder, f'{basename}_explained_variance_ratios.csv'), explained_variance_ratios, delimiter=',')
+
+
+def plot_3d_pca(finalDf, targets, colors, folder, basename):
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlabel('Principal Component 1', fontsize=15)
+    ax.set_ylabel('Principal Component 2', fontsize=15)
+    ax.set_zlabel('Principal Component 3', fontsize=15)
+    ax.set_title('3 component PCA', fontsize=20)
+
+    for target, color in zip(targets, colors):
+        indicesToKeep = finalDf['key'] == target
+        ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1'],
+                   finalDf.loc[indicesToKeep, 'principal component 2'],
+                   finalDf.loc[indicesToKeep, 'principal component 3'],
+                   color=color,
+                   s=50)
+
+    ax.legend(targets)
+    ax.grid()
+
+    os.makedirs(folder, exist_ok=True)
+
+    counter = 1
+    filename = f'{basename}.png'
+    while os.path.exists(os.path.join(folder, filename)):
+        counter += 1
+        filename = f'{basename}_{counter}.png'
+
+    fig.savefig(os.path.join(folder, filename))
+
+    pca_output = finalDf[['principal component 1', 'principal component 2', 'principal component 3']].values
+    np.savetxt(os.path.join(folder, f'{basename}_pca_output.csv'), pca_output, delimiter=',')
+
+    explained_variance_ratios = pca.explained_variance_ratio_
+    np.savetxt(os.path.join(folder, f'{basename}_explained_variance_ratios.csv'), explained_variance_ratios, delimiter=',')
+
+
+def main(input_folder, results_directory):
+    data = read_data(input_folder)
+    df = pd.DataFrame()
+
+    for key, value in data.items():
+        for sublist in value:
+            row = sublist + [key]
+            df = df.append(pd.Series(row), ignore_index=True)
+
+    df = preprocess_data(df)
+
+    features = ['feature1', 'feature2', 'feature3', 'feature4', 'feature5', 'feature6', 'feature7', 'feature8', 'feature9']
+    targets = ['university professor', 'primary school student', 'William Shakespeare', 'Steve Irwin', 'Sherlock Holmes', 'Queen Elizabeth', 'Winston Churchill', 'J.R.R. Tolkien', 'Forrest Gump', 'Human']
+    colors = [(1, 0.4, 0.2, 0.5), (1, 0.2, 1, 0.5), (0.4, 1, 0.2, 0.5), (0.2, 1, 0.8, 0.5), (0.8, 0.2, 1, 0.5), (1, 0.8, 0.2, 0.5), (0.2, 0.4, 1, 0.5), (0.2, 0.8, 1, 0.5), (0.8, 0.2, 0.8, 0.5), (0, 0, 0, 0.3)]
+
+    x = df.loc[:, features].values
+    y = df.loc[:, ['key']].values
+
+    pca = PCA(n_components=2)
+    principalComponents = pca.fit_transform(x)
+    principalDf = pd.DataFrame(data=principalComponents, columns=['principal component 1', 'principal component 2'])
+    finalDf = pd.concat([principalDf, df[['key']]], axis=1)
+    plot_2d_pca(finalDf, targets, colors, results_directory, '2D_PCA_1')
+
+    pca = PCA(n_components=3)
+    principalComponents = pca.fit_transform(x)
+    principalDf = pd.DataFrame(data=principalComponents, columns=[f'principal component {i+1}' for i in range(pca.n_components_)])
+    finalDf = pd.concat([principalDf, df[['key']]], axis=1)
+    plot_3d_pca(finalDf, targets, colors, results_directory, '3D_PCA_1')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--i", "--input_directory", help="Path to the input directory")
+    parser.add_argument("--r", "--results_directory", help="Path to the results directory")
+    args = parser.parse_args()
+
+    if not args.i or not args.r:
+        parser.error("Both input_directory and results_directory are required.")
+
+    print("Beginning Clustering")
+
+    main(args.i, args.r)
---
+++ main.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+import subprocess
+
+def execute_script(script_name, input_directory, output_directory):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    script_path = os.path.join(current_directory, script_name)
+    try:
+        subprocess.call(['python', script_path, '--i', input_directory, '--o', output_directory])
+    except Exception as e:
+        print(f"Error executing script: {e}")
+
+def execute_another_script(script_name, output_directory, results_directory):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    script_path = os.path.join(current_directory, script_name)
+    try:
+        subprocess.call(['python', script_path, '--o', output_directory, '--r', results_directory])
+    except Exception as e:
+        print(f"Error executing script: {e}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Feature Extractor')
+    parser.add_argument('--i', type=str, help='Input directory path')
+    parser.add_argument('--o', type=str, help='Output directory path')
+    parser.add_argument('--r', type=str, help='Results directory path')
+    parser.add_argument('--feature_extracter', help='Name of the feature extractor script')
+    parser.add_argument('--clustering', help='Name of the clustering script')
+    parser.add_argument('--mlp', help='Name of the MLP script')
+    args = parser.parse_args()
+
+    # Check if output and results directories are provided, otherwise create folders "output" and "results"
+    current_directory = os.path.abspath('.')
+    output_directory = args.o if args.o else os.path.join(current_directory, "output")
+    results_directory = args.r if args.r else os.path.join(current_directory, "results")
+
+    if not args.i:
+        print("Error: Please provide the input directory.")
+        exit(1)
+
+    if not os.path.exists(args.i):
+        print("Error: Input directory does not exist.")
+        exit(1)
+
+    if os.path.exists(output_directory) and not args.o:
+        print("Warning: Output directory already exists. Specify a different output directory or use '--o' to overwrite.")
+        exit(1)
+
+    if os.path.exists(results_directory) and not args.r:
+        print("Warning: Results directory already exists. Specify a different results directory or use '--r' to overwrite.")
+        exit(1)
+
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    if not os.path.exists(results_directory):
+        os.makedirs(results_directory)
+
+    if args.feature_extracter:
+        execute_script(args.feature_extracter, args.i, output_directory)
+    else:
+        execute_script('feature_extracter.py', args.i, output_directory)
+
+'''
+    if args.clustering:
+        execute_another_script(args.clustering, output_directory, results_directory)
+    else:
+        execute_another_script('clustering.py', output_directory, results_directory)
+
+    if args.mlp:
+        execute_another_script(args.mlp, output_directory, results_directory)
+    else:
+        execute_another_script('mlp.py', output_directory, results_directory)
+'''
